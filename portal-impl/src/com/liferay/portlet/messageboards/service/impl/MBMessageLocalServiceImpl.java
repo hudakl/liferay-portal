b9f15f0e58d1c4e09307ed59086a7449a49409e8
Roberto Díaz <roberto.diaz@liferay.com>
2017-12-26 12:08:48 +0100

    LPS-76663 We should only modify the thread once the message has pass the workflow process. We should always update the thread when the message is updated.

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
@@ -1557,9 +1557,9 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 			thread.setPriority(priority);
 
 			updatePriorities(thread.getThreadId(), priority);
-		}
 
-		mbThreadPersistence.update(thread);
+			mbThreadPersistence.update(thread);
+		}
 
 		// Asset
 
@@ -2030,10 +2030,6 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 			thread.setStatusByUserId(user.getUserId());
 			thread.setStatusByUserName(user.getFullName());
 			thread.setStatusDate(modifiedDate);
-		}
-
-		if (status == oldStatus) {
-			return;
 		}
 
 		if (status == WorkflowConstants.STATUS_APPROVED) {
