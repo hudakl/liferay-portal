873a4dee5120981cc6f828b9c8534e8985b25abf
hudakl <laszlo.hudak@liferay.com>
2018-10-23 12:54:40 +0200

    LPS-84677 Add FinderImpl class

---
+++ modules/apps/sample-custom-query/sample-custom-query-service/src/main/java/com/liferay/sample/custom/query/service/persistence/impl/CustomFinderImpl.java
@@ -0,0 +1,8 @@
+package com.liferay.sample.custom.query.service.persistence.impl;
+
+import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.sample.custom.query.model.Custom;
+
+public class CustomFinderImpl extends BasePersistenceImpl<Custom> {
+
+}
