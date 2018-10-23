bb14baff1261e88ef1c76a603a2bf3dbb61658cd
hudakl <laszlo.hudak@liferay.com>
2018-10-23 13:18:51 +0200

    LPS-84677 Add finder method

--- modules/apps/sample-custom-query/sample-custom-query-service/src/main/java/com/liferay/sample/custom/query/service/persistence/impl/CustomFinderImpl.java
+++ modules/apps/sample-custom-query/sample-custom-query-service/src/main/java/com/liferay/sample/custom/query/service/persistence/impl/CustomFinderImpl.java
@@ -1,8 +1,40 @@
 package com.liferay.sample.custom.query.service.persistence.impl;
 
-import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
-import com.liferay.sample.custom.query.model.Custom;
+import java.util.List;
+
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
 
 public class CustomFinderImpl extends CustomFinderBaseImpl {
 
+	public List<String> getCustomNames(String sqlQuery) {
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			SQLQuery queryObject = session.createSQLQuery(sqlQuery);
+
+			queryObject.setCacheable(true);
+
+			QueryPos qPos = QueryPos.getInstance(queryObject);
+	
+			for (int i = 0; i < params.length; i++) {
+				if (params[i] != null)
+					qPos.add(params[i]);
+			}
+
+			List<String> result = queryObject.list();
+			return result;
+		} 
+		catch (Exception e) {
+			log.error(FormattingUtil.getMessage(e));
+		} 
+		finally {
+			closeSession(session);
+		}
+
+		return null;
+	}
 }
