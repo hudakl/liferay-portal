4c66b2c0ac57a06a3b1ca271367fd74b31d7c560
hudakl <laszlo.hudak@liferay.com>
2020-03-19 16:42:31 +0100

    LPS-106586 Add test

--- modules/apps/configuration-admin/configuration-admin-web/src/test/java/com/liferay/configuration/admin/web/internal/util/ConfigurationExportImportTest.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/test/java/com/liferay/configuration/admin/web/internal/util/ConfigurationExportImportTest.java
@@ -17,11 +17,13 @@ package com.liferay.configuration.admin.web.internal.util;
 import com.liferay.configuration.admin.web.internal.exporter.ConfigurationExporter;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.kernel.util.ArrayUtil;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.apache.felix.cm.file.ConfigurationHandler;
+import org.apache.felix.utils.properties.TypedProperties;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -90,6 +92,53 @@ public class ConfigurationExportImportTest {
 			arrayValues, (String[])dictionary.get(arrayKey));
 	}
 
+	@Test
+	public void testExportImportTypedProperty() throws Exception {
+		_dictionary.put("typedKey1", "${bar}");
+		_dictionary.put("typedKey2", "${foo.${bar}}");
+		_dictionary.put("typedKey3", "${foo");
+		_dictionary.put("typedKey4", "${foo.${bar}");
+		_dictionary.put("typedKey5", "}${foo.${bar");
+
+		Dictionary<String, Object> escapedDictionary = new Hashtable<>();
+
+		escapedDictionary.put(
+			"typedKey1", ConfigurationExporter.escapeProperty("${bar}"));
+		escapedDictionary.put(
+			"typedKey2", ConfigurationExporter.escapeProperty("${foo.${bar}}"));
+		escapedDictionary.put(
+			"typedKey3", ConfigurationExporter.escapeProperty("${foo"));
+		escapedDictionary.put(
+			"typedKey4", ConfigurationExporter.escapeProperty("${foo.${bar}"));
+		escapedDictionary.put(
+			"typedKey5", ConfigurationExporter.escapeProperty("}${foo.${bar"));
+
+		Dictionary<String, Object> dictionary = _exportImportTypedProperties(
+			escapedDictionary);
+
+		Assert.assertEquals(_dictionary, dictionary);
+	}
+
+	@Test
+	public void testExportImportTypedPropertyArray() throws Exception {
+		String arrayKey = "arrayKey";
+		String[] arrayValues = {"${bar}", "${foo.${bar}}"};
+
+		_dictionary.put(arrayKey, arrayValues);
+
+		Dictionary<String, Object> escapedDictionary = new Hashtable<>();
+
+		escapedDictionary.put(
+			arrayKey, ConfigurationExporter.escapeProperties(arrayValues));
+
+		Dictionary<String, Object> dictionary = _exportImportTypedProperties(
+			escapedDictionary);
+
+		Assert.assertArrayEquals(
+			arrayValues,
+			ArrayUtil.toStringArray((Object[])dictionary.get(arrayKey)));
+	}
+
 	@SuppressWarnings("unchecked")
 	private Dictionary<String, Object> _exportImportProperties(
 			Dictionary<String, Object> dictionary)
@@ -98,6 +147,25 @@ public class ConfigurationExportImportTest {
 		byte[] bytes = ConfigurationExporter.getPropertiesAsBytes(dictionary);
 
 		return ConfigurationHandler.read(new UnsyncByteArrayInputStream(bytes));
+	}
+
+	private Dictionary<String, Object> _exportImportTypedProperties(
+			Dictionary<String, Object> dictionary)
+		throws Exception {
+
+		byte[] bytes = ConfigurationExporter.getPropertiesAsBytes(dictionary);
+
+		TypedProperties typedProperties = new TypedProperties();
+
+		typedProperties.load(new UnsyncByteArrayInputStream(bytes));
+
+		Hashtable<String, Object> importedDictionary = new Hashtable<>();
+
+		for (String key : typedProperties.keySet()) {
+			importedDictionary.put(key, typedProperties.get(key));
+		}
+
+		return importedDictionary;
 	}
 
 	private Dictionary<String, Object> _dictionary;
